71a563fefc50fd4f14697c08400c247209e13608
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2019-10-06 15:12:23 -0300

    Rota admin ajustada

--- index.php
+++ index.php
@@ -4,13 +4,20 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Andredss\Page;
+use \Andredss\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {    
+$app->get('/', function() {   
 	$page = new Page();
+
+	$page->setTpl("index");
+});
+
+$app->get('/admin', function() {
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
