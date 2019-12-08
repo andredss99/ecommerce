161531208c87f286967a67c41783864a1de377e1
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2019-12-07 23:27:30 -0300

    Categories CRUD

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Andredss\Page;
 use \Andredss\PageAdmin;
 use \Andredss\Model\User;
+use \Andredss\Model\Category;
 
 $app = new Slim();
 
@@ -174,7 +175,7 @@ $app->post("/admin/forgot/reset", function() {
 	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
 		"cost" => 12
 	]);
-	
+
 	$user->setPassword($password);
 
 	$page = new PageAdmin([
@@ -183,6 +184,75 @@ $app->post("/admin/forgot/reset", function() {
 	]);
 
 	$page->setTpl("forgot-reset-success");
+});
+
+$app->get("/admin/categories", function() {
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories' => $categories
+	]);
+});
+
+$app->get("/admin/categories/create", function() {
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function() {
+	User::verifyLogin();
+
+	$category = new Category();
+	$category->setData($_POST);
+	$category->save();
+
+	header("location: /index.php/admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory) {
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+	$category->delete();
+
+	header("location: /index.php/admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	$category->get((int)$idcategory);
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category' => $category->getValues()
+	]);
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+	$category->save();
+
+	header("location: /index.php/admin/categories");
+	exit;
 });
 
 $app->run();
