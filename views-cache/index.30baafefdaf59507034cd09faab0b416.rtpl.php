6fbf57a1c2a1c118764829bbcf5994e7543dddbf
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2020-08-05 19:33:42 -0300

    Adiçao da visualizaçao dos detalhes do produto

--- views-cache/index.30baafefdaf59507034cd09faab0b416.rtpl.php
+++ views-cache/index.30baafefdaf59507034cd09faab0b416.rtpl.php
@@ -91,11 +91,11 @@
                                 <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                 <div class="product-hover">
                                     <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Comprar</a>
-                                    <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
+                                    <a href="/index.php/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
                                 </div>
                             </div>
                             
-                            <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
+                            <h2><a href="/index.php/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
 
                             <div class="product-carousel-price">
                                 <ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins>
